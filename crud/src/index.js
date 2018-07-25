120f6ab4e2ca22330532175fc202a6aeeb38e717
tal <tianpengfei1>
2018-07-25 14:22:16 +0800

    import redux,logger,thunk

--- crud/src/index.js
+++ crud/src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux'
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import rootReducer from './reducers'
+import { createStore} from 'redux'
+
+const store = createStore(rootReducer)
+
+ReactDOM.render( 
+    <Provider>
+        <App/>
+    </Provider>,
+    document.getElementById('root'));
+registerServiceWorker()
